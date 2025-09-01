fe2a8b7e847e07dcc75de894152c79f56686e3e0
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-09-01 14:45:48 +0500

    Routes Setup

--- Backend/route/book.route.js
+++ Backend/route/book.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getBook } from "../controller/book.controller.js";
+import { getFreeBook } from "../controller/book.controller.js";
 
 const router = express.Router();
 
 router.get("/", getBook);
+router.get('/free', getFreeBook);
 
 export default router;
